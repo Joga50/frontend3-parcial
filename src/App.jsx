057408fc62b04756695b238e1dee0c373e9097ca
Joga50 <jose.gabriel.a12@gmail.com>
2023-03-07 14:18:13 -0500

    aplicando logica para validacion de inputs con react useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import React from "react";
-
-import Input from "./input";
+import React, { useState } from "react";
+import Card from "./Card";
 
 function App() {
   //Aqui deberias agregar los estados y los handlers para los inputs
+  const [nombre, setNombre] = useState("");
+  const [pelicula, setPelicula] = useState("");
+  const [error, setError] = useState(false);
+  const [enviado, setEnviado] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (nombre.trim().length < 3 || /^\s/.test(nombre) || pelicula.length < 6) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    setEnviado(true);
+  };
+
+  const handleNombreInputChange = (e) => {
+    setNombre(e.target.value);
+  };
+
+  const handlePeliculaInputChange = (e) => {
+    setPelicula(e.target.value);
+  };
 
   return (
     <div className="App">
-      <h1>Elige un color</h1>
-
+      <h1>Elige un color:</h1>
       <form
+        onSubmit={handleSubmit}
         style={{
           backgroundColor: "gray",
           width: "500px",
@@ -19,8 +40,34 @@ function App() {
           alignItems: "center",
         }}
       >
-        <Input />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "300px",
+          }}
+        >
+          <label>Nombre:</label>
+          <input
+            placeholder="Ingresa tu nombre"
+            value={nombre}
+            onChange={handleNombreInputChange}
+            style={{ margin: "15px" }}
+          />
+          <lbael>Color:</lbael>
+          <input
+            value={pelicula}
+            onChange={handlePeliculaInputChange}
+            style={{ margin: "15px" }}
+            placeholder="Ingresa tu color favorito formato HEX"
+          />
+          <button type="submit" style={{ margin: "15px" }}>
+            Enviar
+          </button>
+        </div>
       </form>
+      {error && <p>Porfavor ingresa la informacion correcta</p>}
+      {enviado && <Card nombre={nombre} pelicula={pelicula} />}
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,12 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
+function Card({ nombre, pelicula }) {
   return (
     <div>
-      <h2>Esto es un componente</h2>
-      <input />
+      <h2>Datos recibidos:</h2>
+      <p>Nombre: {nombre}</p>
+      <p>Pelicula: {pelicula}</p>
     </div>
   );
 }
